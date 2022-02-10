e1ea1499885ac4624b199758a2bbbb64799e588b
Manuel Cillero <manuel@cillero.es>
2022-02-10 23:15:50 +0100

    Añade Sycamore para render de HTML con reactividad

--- src/core/server/main.rs
+++ src/core/server/main.rs
@@ -1,15 +1,22 @@
 use crate::core::{server, Server};
 
-async fn greet(req: server::HttpRequest) -> impl server::Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+async fn greet() -> impl server::Responder {
+    "Hello!"
+}
+
+async fn greet_with_param(req: server::HttpRequest) -> server::HttpResponse {
+    let name: String = req.match_info().get("name").unwrap_or("World").into();
+    server::HttpResponse::Ok()
+        .body(sycamore::render_to_string(|ctx| sycamore::view! { ctx,
+            p { "Hello " (name) "!" }
+        }))
 }
 
 pub fn run() -> Result<Server, std::io::Error> {
     let server = server::HttpServer::new(|| {
         server::App::new()
             .route("/", server::web::get().to(greet))
-            .route("/{name}", server::web::get().to(greet))
+            .route("/{name}", server::web::get().to(greet_with_param))
         })
         .bind("127.0.0.1:8000")?
         .run();
--- src/core/server/mod.rs
+++ src/core/server/mod.rs
@@ -1,4 +1,4 @@
-pub use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+pub use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 mod main;
 pub use main::run;
