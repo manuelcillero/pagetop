7ed2d37e52531dbe3cc49e07d86d7be455875e74
Manuel Cillero <manuel@cillero.es>
2023-01-23 20:41:02 +0100

    🔥 Quita código incluido ahora en [pagetop-build]

--- pagetop/build.rs
+++ pagetop/build.rs
@@ -1,19 +1,6 @@
-use std::path::Path;
-
 fn main() -> std::io::Result<()> {
-    bundle_resources("./static/theme", "theme")?;
-    bundle_resources("./static/aliner", "aliner")?;
-    bundle_resources("./static/bootsier", "bootsier")?;
-    bundle_resources("./static/bulmix", "bulmix")
-}
-
-/// This function is a simplified version of `pagetop::util::bundle_resources()`.
-pub fn bundle_resources(from_dir: &str, with_name: &str) -> std::io::Result<()> {
-    let mut bundle = static_files::resource_dir(from_dir);
-    bundle.with_generated_filename(
-        Path::new(std::env::var("OUT_DIR").unwrap().as_str()).join(format!("{}.rs", with_name)),
-    );
-    bundle.with_module_name(format!("resources_{}", with_name));
-    bundle.with_generated_fn(format!("bundle_{}", with_name));
-    bundle.build()
+    pagetop_build::bundle_resources("./static/theme", "theme", None)?;
+    pagetop_build::bundle_resources("./static/aliner", "aliner", None)?;
+    pagetop_build::bundle_resources("./static/bootsier", "bootsier", None)?;
+    pagetop_build::bundle_resources("./static/bulmix", "bulmix", None)
 }
