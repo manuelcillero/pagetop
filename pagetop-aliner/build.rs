fd54389cd515fda18392d8b47738a99c90f4696e
Manuel Cillero <manuel@cillero.es>
2023-02-02 08:45:52 +0100

    🎉 [aliner] Saca tema del código base de PageTop

---
+++ pagetop-aliner/build.rs
@@ -0,0 +1,3 @@
+fn main() -> std::io::Result<()> {
+    pagetop_build::bundle_resources("./static", "aliner", None)
+}
---
+++ pagetop-aliner/src/lib.rs
@@ -0,0 +1,30 @@
+use pagetop::prelude::*;
+
+pub_handle!(THEME_ALINER);
+
+include!(concat!(env!("OUT_DIR"), "/aliner.rs"));
+
+pub struct Aliner;
+
+impl ModuleTrait for Aliner {
+    fn handle(&self) -> Handle {
+        THEME_ALINER
+    }
+
+    fn theme(&self) -> Option<ThemeStaticRef> {
+        Some(&Aliner)
+    }
+
+    fn configure_service(&self, cfg: &mut server::web::ServiceConfig) {
+        serve_static_files!(cfg, "/aliner", bundle_aliner);
+    }
+}
+
+impl ThemeTrait for Aliner {
+    fn before_render_page(&self, page: &mut Page) {
+        page.alter_favicon(Some(Favicon::new().with_icon("/theme/favicon.ico")))
+            .alter_context(ContextOp::AddStyleSheet(
+                StyleSheet::located("/aliner/css/styles.css").with_weight(-99),
+            ));
+    }
+}
