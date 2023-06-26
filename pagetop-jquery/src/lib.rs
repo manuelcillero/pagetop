e033630d2ac1900a4ab00920c3bf39d44cff9682
Manuel Cillero <manuel@cillero.es>
2023-06-26 21:23:48 +0200

    🚧 [jquery] Preparando el módulo para un tutorial

--- pagetop-jquery/src/lib.rs
+++ pagetop-jquery/src/lib.rs
@@ -2,16 +2,29 @@ use pagetop::prelude::*;
 
 use_handle!(MODULE_JQUERY);
 
+use_locale!(LOCALE_JQUERY);
+
 use_static!(jquery);
 
 const JQUERY_PARAM: &str = "jquery.add";
-const JQUERY_SOURCE: &str = "/jquery/3.6.0/jquery.min.js";
 
 pub struct JQuery;
 
 impl ModuleTrait for JQuery {
     fn handle(&self) -> Handle {
         MODULE_JQUERY
+    }
+
+    fn actions(&self) -> Vec<Action> {
+        vec![action!(ActionBeforeRenderPage => before_render_page)]
+    }
+
+    fn name(&self) -> L10n {
+        L10n::t("module_name", &LOCALE_JQUERY)
+    }
+
+    fn description(&self) -> L10n {
+        L10n::t("module_description", &LOCALE_JQUERY)
     }
 
     fn configure_service(&self, cfg: &mut service::web::ServiceConfig) {
@@ -21,23 +34,20 @@ impl ModuleTrait for JQuery {
 
 impl JQuery {
     pub fn add_jquery(rcx: &mut RenderContext) {
-        match rcx.get_param::<bool>(JQUERY_PARAM) {
-            Some(true) => {}
-            _ => {
-                rcx.alter(ContextOp::AddJavaScript(
-                    JavaScript::located(JQUERY_SOURCE)
-                        .with_weight(isize::MIN)
-                        .with_mode(ModeJS::Normal),
-                ));
-                rcx.set_param::<bool>(JQUERY_PARAM, true);
-            }
-        }
+        rcx.set_param::<bool>(JQUERY_PARAM, true);
     }
 
     pub fn remove_jquery(rcx: &mut RenderContext) {
-        if let Some(true) = rcx.get_param::<bool>(JQUERY_PARAM) {
-            rcx.alter(ContextOp::RemoveJavaScript(JQUERY_SOURCE));
-            rcx.set_param::<bool>(JQUERY_PARAM, false);
-        }
+        rcx.set_param::<bool>(JQUERY_PARAM, false);
+    }
+}
+
+fn before_render_page(page: &mut Page) {
+    if let Some(true) = page.context().get_param::<bool>(JQUERY_PARAM) {
+        page.context().alter(ContextOp::AddJavaScript(
+            JavaScript::located("/jquery/3.6.0/jquery.min.js")
+                .with_weight(isize::MIN)
+                .with_mode(ModeJS::Normal),
+        ));
     }
 }
