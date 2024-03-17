21dfe8543b027c15aa51457e214bb41040721be0
Manuel Cillero <manuel@cillero.es>
2024-03-18 00:05:04 +0100

    ⏪ Revert localization of 403 & 404 error components

--- src/base/component.rs
+++ src/base/component.rs
@@ -163,8 +163,11 @@ pub use basic::*;
 mod composition;
 pub use composition::*;
 
-mod error;
-pub use error::*;
+mod error403;
+pub use error403::Error403;
+
+mod error404;
+pub use error404::Error404;
 
 mod heading;
 pub use heading::{Heading, HeadingSize, HeadingType};
--- src/base/component/error.rs
+++
@@ -1,5 +0,0 @@
-mod error403;
-pub use error403::Error403;
-
-mod error404;
-pub use error404::Error404;
